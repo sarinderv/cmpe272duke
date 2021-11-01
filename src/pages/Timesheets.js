1415d75ba62141ebcf44896efac34452e00bdeab
Sarinder Virk <sarinder.virk@oracle.com>
2021-10-31 23:51:48 -0700

    added side navigation bar

---
+++ src/pages/Timesheets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Timesheets() {
+    return (
+        <div className='timesheets'>
+            <h1>Timesheets</h1>
+        </div>
+    );
+}
+
+export default Timesheets;
